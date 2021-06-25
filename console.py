44318f22fed98d231cf876e1dd67f7948976fda7
kateincoding <kateincoding@gmail.com>
2021-06-25 03:47:57 +0000

    [do_create] finished

--- console.py
+++ console.py
@@ -3,14 +3,14 @@
 Class CommandConsole for Airbnb
 """
 import cmd
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """
     HBNB Class
     """
-
     prompt = '(hbnb)'
+    allowed_class = {"BaseModel": BaseModel}
 
     def do_quit(self, line):
         """quit command: exit the program"""
@@ -23,6 +23,20 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """overridden to not do nothing"""
         pass
+
+    def do_create(self, line):
+        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id"""
+        if len(line) == 0:
+            print("** class name missing **")
+            return
+        elif line in HBNBCommand.allowed_class.keys():
+            instance =  HBNBCommand.allowed_class[line]()
+        elif line not in HBNBCommand.allowed_class.keys():
+            print("** class doesn't exist **")
+            return
+
+        print(instance.id)
+        instance.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
