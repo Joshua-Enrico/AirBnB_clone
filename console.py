8a6261ace737ea4fe2b43ffa59e7ca8846b0a7ba
kateincoding <kateincoding@gmail.com>
2021-06-26 16:45:44 +0000

    [default all] advanced 1

--- console.py
+++ console.py
@@ -4,15 +4,25 @@ Class CommandConsole for Airbnb
 """
 import cmd
 from models.base_model import BaseModel
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.user import User
 import models
+import sys
 
-allowed_class = {"BaseModel": BaseModel}
+allowed_class = {"BaseModel": BaseModel, "Place": Place, "State": State,
+                 "City": City, "Amenity": Amenity, "Review": Review,
+                 "User": User}
 
 class HBNBCommand(cmd.Cmd):
     """
     HBNB Class
     """
     prompt = '(hbnb)'
+    __all_117 = 0
 
     def do_quit(self, line):
         """quit command: exit the program"""
@@ -25,6 +35,18 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """overridden to not do nothing"""
         pass
+
+    def precmd(self, line):
+        """ Edit given command to allow second type of input"""
+        if not sys.stdin.isatty():
+            print()
+        if '.' in line:
+            HBNBCommand.__all_117 = 1
+            line = line.replace('.', ' ').replace('(', ' ').replace(')', ' ')
+            cmd_argv = line.split()
+            cmd_argv[0], cmd_argv[1] = cmd_argv[1], cmd_argv[0]
+            line = " ".join(cmd_argv)
+        return cmd.Cmd.precmd(self, line)
 
     def do_create(self, line):
         """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id"""
@@ -97,10 +119,17 @@ class HBNBCommand(cmd.Cmd):
         if cmd_line[0] not in allowed_class.keys():
             print("** class doesn't exist **")
         else:
-            instance = cmd_line[0] + "." + cmd_line[1]
-            if instance in models.storage.all():
+            # instance = cmd_line[0] + "." + cmd_line[1]
+            # if instance in models.storage.all():
+            if len(cmd_line) > 1:
+                instance = cmd_line[0] + "." + cmd_line[1]
+                if instance in models.storage.all():
+                    print('["', end="")
+                    print(models.storage.all()[instance], end="")
+                    print('"]')
+            else:
                 print('["', end="")
-                print(models.storage.all()[instance], end="")
+                print(models.storage.all(), end="")
                 print('"]')
 
     def do_update(self, line):
@@ -128,6 +157,13 @@ class HBNBCommand(cmd.Cmd):
                 select_obj = models.storage.all().get(instance)
                 setattr(select_obj, cmd_line[2], cmd_line[3][1:-1])
                 select_obj.save()
+
+    def default(self, line):
+        """overriden a command when the command doesn't exist"""
+        my_list = line.split('.(,)')
+        if len(my_list) >= 2:
+            if my_list[1] == "all()":
+                self.do_all(my_list[0])
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
