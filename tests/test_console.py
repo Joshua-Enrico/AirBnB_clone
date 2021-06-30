04eb702e87d913ce85c91bc2e10f74b7496534a7
kateincoding <kateincoding@gmail.com>
2021-06-30 15:16:56 +0000

    [help test] added

--- tests/test_console.py
+++ tests/test_console.py
@@ -62,6 +62,110 @@ class ConsoleTest(unittest.TestCase):
             self.assertEqual('*** Unknown syntax: goku\n',
                              f.getvalue())
 
+
+class HelpTest(unittest.TestCase):
+    """testing command test in console"""
+
+    @classmethod
+    def setUpClass(self):
+        """setting class up"""
+        self.console = HBNBCommand()
+
+    def help_command(self):
+        """testing an only help command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help")
+            output = '\nDocumented commands (type help <topic>)[103 chars]\n\n'
+            self.assertEqual(output, f.getvalue())
+
+    def help_help_command(self):
+        """testing an only help command"""
+        expected = 'List available commands with "help" or \
+            detailed help with "help cmd".\n'
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help help")
+            self.assertEqual(expected, f.getvalue())
+
+    def EOF_help_command(self):
+        """testing an only help command"""
+        expected = 'End of File command: exit the program\n'
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help EOF")
+            self.assertEqual(expected, f.getvalue())
+
+    def all_help_command(self):
+        """testing an only help command"""
+        expected = 'Prints all string representation of all instances \
+            based or not on the class name \
+            Ex: $ all BaseModel or $ all.\n'
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help all")
+            self.assertEqual(expected, f.getvalue())
+
+    def count_help_command(self):
+        """testing an only help command"""
+        expected = 'count instances of the class\n'
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help count")
+            self.assertEqual(expected, f.getvalue())
+
+    def create_help_command(self):
+        """testing an only help command"""
+        expected = 'Creates a new instance of BaseModel, saves it (to the JSON file)\n \
+            and prints the id\n'
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help create")
+            self.assertEqual(expected, f.getvalue())
+
+    def quit_help_command(self):
+        """testing an only help command"""
+        expected = 'quit command: exit the program\n'
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help quit")
+            self.assertEqual(expected, f.getvalue())
+
+    def destroy_help_command(self):
+        """testing an only help command"""
+        expected = 'Deletes an instance based on the class name and\n \
+        id (save the change into the JSON file).\n \
+        Ex: $ destroy BaseModel 1234-1234-1234\n'
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help destroy")
+            self.assertEqual(expected, f.getvalue())
+
+    def show_help_command(self):
+        """testing an only help command"""
+        expected = 'Prints the string representation of an instance\n \
+            based on the class name and id.\n \
+            Ex: $ show BaseModel 1234-1234-1234.'
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help show")
+            self.assertEqual(expected, f.getvalue())
+
+    def update_help_command(self):
+        """testing an only help command"""
+        expected = 'Updates an instance based on the class name and id\n \
+            by adding or updating attribute\n \
+            (save the change into the JSON file).\n \
+            - Usage:\n \
+            update <class name> <id> <attribute name> "<attribute value>"\n \
+            - Ex:\n \
+            $ update BaseModel 1234-1234-1234 email\
+                 "aibnb@holbertonschool.com"\n \
+            - Only one attribute can be updated at the time\n'
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help update")
+            self.assertEqual(expected, f.getvalue())
+
+
+class CreateTest(unittest.TestCase):
+    """testing command test in console"""
+
+    @classmethod
+    def setUpClass(self):
+        """setting class up"""
+        self.console = HBNBCommand()
+
     def test_create(self):
         """testing creat input"""
         with patch('sys.stdout', new=StringIO()) as f:
@@ -79,6 +183,15 @@ class ConsoleTest(unittest.TestCase):
             self.console.onecmd("all User")
             self.assertEqual(
                             '["[User', f.getvalue()[:7])
+
+
+class ShowTest(unittest.TestCase):
+    """testing command show in console"""
+
+    @classmethod
+    def setUpClass(self):
+        """setting class up"""
+        self.console = HBNBCommand()
 
     def test_show(self):
         """testing show's behaviour"""
